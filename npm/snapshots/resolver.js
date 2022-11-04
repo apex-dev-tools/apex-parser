f2cea9995038b4e6daaa8d7e37f46b220db60722
Peter Wright <pwright@financialforce.com>
2022-11-04 14:23:50 +0000

    test: Fix snapshot resolution and prevent regular test running system test

--- npm/snapshots/resolver.js
+++ npm/snapshots/resolver.js
@@ -1,12 +1,12 @@
 module.exports = {
 	// resolves from test to snapshot path
 	resolveSnapshotPath: (testPath, snapshotExtension) =>
-		testPath.replace('__tests__', '../snapshots') + snapshotExtension,
+		testPath.replace('lib/__tests__', 'snapshots') + snapshotExtension,
 
 	// resolves from snapshot to test path
 	resolveTestPath: (snapshotFilePath, snapshotExtension) =>
 		snapshotFilePath
-			.replace('../snapshots', '__tests__')
+			.replace('snapshots', 'lib/__tests__')
 			.slice(0, -snapshotExtension.length),
 
 	// Example test path, used for preflight consistency check of the implementation above
